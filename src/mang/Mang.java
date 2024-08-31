09ac48502f282cb4497add2e413141e6c524f8de
TrungKien02062005 <lekien020605@gmail.com>
2024-08-31 10:02:37 +0700

    commit

---
+++ src/mang/Mang.java
@@ -0,0 +1,19 @@
+
+package mang;
+
+import java.util.Arrays;
+
+public class Mang {
+
+    
+    public static void main(String[] args) {
+    int[] a1= {4,15,8};
+    int[] a2= a1; 
+    a2[0] = 7; 
+        System.out.println(Arrays.toString(a2));
+    }
+    
+    
+}
+    
+
